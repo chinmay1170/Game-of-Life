91ec49a2d0279da378a1d2824f4ad14f118ebef3
Chinmay1170 <chinmaykrishna3@gmail.com>
2020-11-05 14:10:47 +0530

    Updates

---
+++ precache-manifest.2ec688d049dcee844ee8c04bcd429784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f285944b205348f843db7c9a024eb1d",
+    "url": "/Game-of-Life/index.html"
+  },
+  {
+    "revision": "18adb9f7a5127bc97c35",
+    "url": "/Game-of-Life/static/css/main.a529e3cd.chunk.css"
+  },
+  {
+    "revision": "c7ab422f1296cc5841cf",
+    "url": "/Game-of-Life/static/js/2.ec745e62.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Game-of-Life/static/js/2.ec745e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18adb9f7a5127bc97c35",
+    "url": "/Game-of-Life/static/js/main.7f9cffe3.chunk.js"
+  },
+  {
+    "revision": "04d0c8dafba4a56d2927",
+    "url": "/Game-of-Life/static/js/runtime-main.40fbf1ce.js"
+  }
+]);
